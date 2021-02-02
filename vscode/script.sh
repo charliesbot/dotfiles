494605d59f3166414f97499257229984e0a3aa25
Charlie L <charliesbot@gmail.com>
2021-02-01 16:22:16 -0800

    remove unused files

--- vscode/script.sh
+++
@@ -1,5 +0,0 @@
-rm -rf ~/Library/Application Support/Code - Insiders/User/settings.json
-rm -rf ~/Library/Application Support/Code - Insiders/User/keybindings.json
-
-ln -s ~/dotfiles/vscode/settings.json
-ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application Support/Code - Insiders/User/keybindings.json
