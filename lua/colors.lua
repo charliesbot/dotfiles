df379fa9f540209dc56949dee6e64a8eb6cfc62d
Charlie L <charliesbot@gmail.com>
2022-12-29 18:26:20 -0600

    cleanup

--- lua/colors.lua
+++
@@ -1,9 +0,0 @@
-vim.g.gruvbox_italic = 1
-vim.g.gruvbox_material_background = 'hard'
-vim.g.gruvbox_material_enable_italic = 1
-vim.g.dracula_italic = 1
-
-vim.cmd('set background=dark')
-vim.cmd('set t_ut=')
-
-vim.cmd('highlight clear SignColumn')
