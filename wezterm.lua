11488e7aa71673d8c5ed993770480941293a4570
Charlie L <charliesbot@gmail.com>
2023-01-28 22:35:23 -0800

    add wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require "wezterm"
+
+return {
+  font = wezterm.font({
+    family = "MonoLisa",
+    harfbuzz_features = {
+      "ss02=1",
+    },
+  }),
+  font_size = 15,
+  color_scheme = "Catppuccin Mocha",
+  hide_tab_bar_if_only_one_tab = true
+}
