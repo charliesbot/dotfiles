8c3fc9f08d4985659457b4af0f76c53ce7114042
charliesbot <charliesbot@@gmail.com>
2024-09-01 19:46:51 -0700

    update dotfiles

--- install.sh
+++ install.sh
@@ -1,24 +1,35 @@
 #!/usr/bin/env bash
-if [[ $(uname) == "Darwin" ]]; then
-	echo "Mac detected. Using Mac config..."
-	curl -sS https://starship.rs/install.sh | sh
-	echo "Installing brew"
-	# install brew
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+os_type=$(uname)
+
+if [[ $os_type == "Darwin" ]]; then
+    config_type="Mac"
+else
+    config_type="Linux"
 fi
 
+echo "$config_type detected. Using $config_type config..."
+
+# Brew must be installed before any other step
+echo "Installing brew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 if [[ $(uname) == "Linux" ]]; then
-	echo "Linux detected. Using Linux config..."
-	echo "Bluefin already supports Starship."
+	sudo dnf upgrade -y
+	sudo dnf install -y zsh curl wget git
+	(
+		echo
+		echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
+	) >>$HOME/.bashrc
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 	echo "Switching to ZSH"
 fi
 
-echo "Installing Kitty Snazzy Theme"
-curl -o ~/.config/kitty/snazzy.conf https://raw.githubusercontent.com/connorholyday/kitty-snazzy/master/snazzy.conf
+echo "Installing Starship"
+curl -sS https://starship.rs/install.sh | sh
 
 echo "Removing existing dotfiles"
 # remove files if they already exist
-rm -rf ~/.vim ~/.vimrc ~/.zshrc ~/.tmux ~/.tmux.conf ~/.config/nvim 2>/dev/null
+rm -rf ~/.vim ~/.vimrc ~/.zshrc ~/.config/nvim 2>/dev/null
 rm -rf ~/.ideavimrc
 
 echo "Creating symlinks"
@@ -27,38 +38,32 @@ mkdir -p ~/.config/ ~/.config/nvim/
 
 # Symlinking files
 ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/nvim/* ~/.config/nvim/
 ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
 ln -s ~/dotfiles/wezterm.lua ~/.wezterm.lua
 ln -s ~/dotfiles/ideavimrc ~/.ideavimrc
 
-# Italics and true color profile for tmux
-tic -x tmux.terminfo
-
 brew update
 
-brew install ripgrep
-brew install tmux
+brew install zellij
+brew install jesseduffield/lazydocker/lazydocker
+brew install lazydocker
 brew install neovim
-brew install ag
 brew install fzf
-brew install bat
-brew install go
-brew install llvm
-brew install gcc
-brew install gdb
-brew install bazel
-brew install cmake
-brew install pyenv
 brew install nvm
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
 
 if [[ $(uname) == "Linux" ]]; then
-	echo "Linux detected. Using Linux config..."
+	mkdir -p ~/.local/share/fonts
 	echo "Installing JetBrains Mono"
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+	echo "Installing Cascadia"
+	wget -qO- https://github.com/microsoft/cascadia-code/releases/download/v2404.23/CascadiaCode-2404.23.zip -O cascadia.zip
+	unzip -o cascadia.zip -d ~/.local/share/fonts
+	rm cascadia.zip
+	fc-cache -fv
+	echo "Installing WezTerm"
 	flatpak run org.wezfurlong.wezterm -y
 fi
 
@@ -67,13 +72,12 @@ if [[ $(uname) == "Darwin" ]]; then
 	# disable key repeat
 	defaults write -g ApplePressAndHoldEnabled -bool false
 
-	brew install starship
-
 	brew tap homebrew/cask-fonts
 
 	# casks only work in mac
-	brew install --cask kitty
+	echo "Installing Cascadia"
 	brew install --cask font-cascadia
+	echo "Installing JetBrains Mono"
 	brew install --cask font-jetbrains-mono
 
 	brew install reattach-to-user-namespace
@@ -82,21 +86,29 @@ fi
 # FZF shortcuts
 $(brew --prefix)/opt/fzf/install
 
-# Go setup
-mkdir -p $HOME/go/{bin,src,pkg}
-
 # Writting vim will launch nvim
 alias vim="nvim"
 
 # Check if the current shell is already zsh
-if [ "$SHELL" = "/usr/bin/zsh" ]; then
-	echo "DONE!"
+if [[ "$SHELL" == *"zsh" ]]; then
+    echo "You are already using zsh as your default shell."
 else
-        if [[ $(uname) == "Linux" ]]; then
-		just shell zsh
-		# Change the default shell to zsh for future logins
-		echo "Log out and back in to use zsh as your default shell."
-	fi
+    # Get the path of zsh
+    zsh_path=$(which zsh)
+
+    # Change the default shell to zsh for future logins
+    echo "Setting up zsh as your default shell..."
+    if chsh -s "$zsh_path"; then
+        echo "Setup complete. Log out and back in to start using zsh as your default shell."
+    else
+        echo "Error: Failed to change the default shell."
+	echo "Please try running 'chsh -s $(which zsh)' manually."
+    fi
 fi
 
-echo "Setup complete."
+echo "All systems operational. 🤖"
+echo " ┓     ┓•   ┓    "
+echo "┏┣┓┏┓┏┓┃┓┏┓┏┣┓┏┓╋"
+echo "┗┛┗┗┻┛ ┗┗┗ ┛┗┛┗┛┗"
+echo "                 "
+echo "Your development environment is ready! Blast off!"
