088f5dfb41804d721727c8b1edfa12c65c05343b
Charlie L <charliesbot@gmail.com>
2025-08-10 17:02:33 -0700

    bring back nvim in my dotfiles

---
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,102 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {
+      close_if_last_window = true,
+      popup_border_style = 'rounded',
+      enable_git_status = true,
+      enable_diagnostics = true,
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            -- Change type
+            added = '✚',
+            modified = '',
+            deleted = '✖',
+            renamed = '󰁕',
+            -- Status type
+            untracked = '',
+            ignored = '',
+            unstaged = '󰄱',
+            staged = '',
+            conflict = '',
+          },
+        },
+      },
+      window = {
+        position = 'left',
+        width = 30,
+        mapping_options = {
+          noremap = true,
+          nowait = true,
+        },
+        mappings = {
+          ['<space>'] = {
+            'toggle_node',
+            nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+          },
+          ['<2-LeftMouse>'] = 'open',
+          ['<cr>'] = 'open',
+          ['<esc>'] = 'cancel', -- close preview or floating neo-tree window
+          ['P'] = { 'toggle_preview', config = { use_float = true, use_image_nvim = true } },
+          ['l'] = 'focus_preview',
+          ['S'] = 'open_split',
+          ['s'] = 'open_vsplit',
+          ['t'] = 'open_tabnew',
+          ['w'] = 'open_with_window_picker',
+          ['C'] = 'close_node',
+          ['z'] = 'close_all_nodes',
+          ['a'] = {
+            'add',
+            config = {
+              show_path = 'none', -- "none", "relative", "absolute"
+            },
+          },
+          ['A'] = 'add_directory',
+          ['d'] = 'delete',
+          ['r'] = 'rename',
+          ['y'] = 'copy_to_clipboard',
+          ['x'] = 'cut_to_clipboard',
+          ['p'] = 'paste_from_clipboard',
+          ['c'] = 'copy',
+          ['m'] = 'move',
+          ['q'] = 'close_window',
+          ['<C-b>'] = 'close_window',
+          ['R'] = 'refresh',
+          ['?'] = 'show_help',
+          ['<'] = 'prev_source',
+          ['>'] = 'next_source',
+          ['i'] = 'show_file_details',
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This needs to be true to show hidden files
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+        follow_current_file = {
+          enabled = true,
+        },
+        hijack_netrw_behavior = 'open_default',
+        use_libuv_file_watcher = true,
+      },
+      git_status = {
+        window = {
+          position = 'float',
+        },
+      },
+    }
+
+    vim.keymap.set('n', '<C-b>', ':Neotree toggle<CR>', { desc = 'Toggle Neo-tree', silent = true })
+  end,
+}
---
+++ nvim/lua/kickstart/plugins/opencode.lua
@@ -0,0 +1,20 @@
+return {
+  'NickvanDyke/opencode.nvim',
+  dependencies = {
+    'folke/snacks.nvim',
+  },
+  ---@type opencode.Config
+  opts = {
+    -- Your configuration, if any
+  },
+  -- stylua: ignore
+  keys = {
+    -- opencode.nvim exposes a general, flexible API — customize it to your workflow!
+    -- But here are some examples to get you started :)
+    { '<leader>ot', function() require('opencode').toggle() end, desc = 'Toggle opencode', },
+    { '<leader>oa', function() require('opencode').ask() end, desc = 'Ask opencode', mode = { 'n', 'v' }, },
+    { '<leader>oA', function() require('opencode').ask('@file ') end, desc = 'Ask opencode about current file', mode = { 'n', 'v' }, },
+    { '<leader>on', function() require('opencode').command('/new') end, desc = 'New session', },
+    { '<leader>oe', function() require('opencode').prompt('Explain @cursor and its context') end, desc = 'Explain code near cursor' },
+  },
+}
