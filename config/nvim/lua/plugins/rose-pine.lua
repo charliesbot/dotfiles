35f6aae8dc8f78be08b7aee4ae823deca4288b43
Charlie L <charliesbot@gmail.com>
2026-07-04 18:19:07 -0700

    nvim: add tokyonight and rose-pine themes

---
+++ config/nvim/lua/plugins/rose-pine.lua
@@ -0,0 +1,10 @@
+-- Extra colorscheme, installed and available via :colorscheme rose-pine.
+-- Not activated: catppuccin remains the default (see catppuccin.lua).
+
+return {
+  'rose-pine/neovim',
+  name = 'rose-pine',
+  lazy = false,
+  priority = 1000,
+  opts = {},
+}
---
+++ config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,9 @@
+-- Extra colorscheme, installed and available via :colorscheme tokyonight.
+-- Not activated: catppuccin remains the default (see catppuccin.lua).
+
+return {
+  'folke/tokyonight.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {},
+}
